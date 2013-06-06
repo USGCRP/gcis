7c1afce9da13cfd6c5bcdd9d06a0606ddb8f94df
Brian Duggan <bduggan@usgcrp.gov>
2013-06-06 12:37:10 -0400

    dbpatch for comments

---
+++ db/patches/0050_comments.sql
@@ -0,0 +1,5 @@
+comment on column report.identifier is 'A unique identifier for the report.';
+comment on column chapter.identifier is 'A unique identifier for the chapter.';
+comment on column figure.identifier is 'A unique identifier for the figure.';
+comment on column image.identifier is 'A unique identifier for the image.';
+
