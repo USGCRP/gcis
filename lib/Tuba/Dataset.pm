218ab55d987838f423f13c8e85c08a5c4b83c969
Brian Duggan <bduggan@usgcrp.gov>
2013-06-12 09:59:39 -0400

    link to findings, datasets

--- lib/Tuba/Dataset.pm
+++ lib/Tuba/Dataset.pm
@@ -18,5 +18,20 @@ sub list {
     );
 }
 
+sub show {
+    my $c = shift;
+    my $meta = Dataset->meta;
+    my $identifier = $c->stash('dataset_identifier');
+    my $object =
+      Dataset->new( identifier => $identifier )->load( speculative => 1)
+      or return $c->render_not_found;
+    $c->stash(object => $object);
+    $c->respond_to(
+        json => sub { shift->render(json => $object->as_tree) },
+        html => sub { shift->render(template => 'object', meta => $meta, objects => $object ) }
+    );
+}
+
+
 1;
 
