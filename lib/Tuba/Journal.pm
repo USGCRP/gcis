d1a55e6e2fd466442041be93a4d4bab84ad368da
Brian Duggan <bduggan@usgcrp.gov>
2013-06-19 12:54:27 -0400

    journals

---
+++ lib/Tuba/Journal.pm
@@ -0,0 +1,36 @@
+=head1 NAME
+
+Tuba::Journal : Controller class for journals.
+
+=cut
+
+package Tuba::Journal;
+use Mojo::Base qw/Tuba::Controller/;
+use Tuba::DB::Objects qw/-nicknames/;
+
+sub list {
+    my $c = shift;
+    my $objects = Journals->get_objects;
+    my $meta = Journal->meta;
+    $c->respond_to(
+        json => sub { shift->render(json => [ map $_->as_tree, @$objects ]) },
+        html => sub { shift->render(template => 'objects', meta => $meta, objects => $objects ) }
+    );
+}
+
+sub show {
+    my $c = shift;
+    my $meta = Journal->meta;
+    my $identifier = $c->stash('journal_identifier');
+    my $object =
+      Journal->new( identifier => $identifier )->load( speculative => 1)
+      or return $c->render_not_found;
+    $c->stash(object => $object);
+    $c->respond_to(
+        json => sub { shift->render(json => $object->as_tree) },
+        html => sub { shift->render(template => 'object', meta => $meta, objects => $object ) }
+    );
+}
+
+1;
+
