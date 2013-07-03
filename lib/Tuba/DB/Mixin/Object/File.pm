1d893a02301bdbb27f5d86e5d1450c0ef21f4d2a
Brian Duggan <bduggan@usgcrp.gov>
2013-07-03 11:50:28 -0400

    file thumbnail

---
+++ lib/Tuba/DB/Mixin/Object/File.pm
@@ -0,0 +1,13 @@
+package Tuba::DB::Object::File;
+use Mojo::ByteStream qw/b/;
+use strict;
+# Tuba::DB::Mixin::Object::File;
+
+sub thumbnail {
+    my $s = shift;
+    my $c = shift || Carp::confess 'foo';
+    return $c->image( $c->base.'/img/'.$s->file, width =>"100px", height => "100px" );
+}
+
+1;
+
