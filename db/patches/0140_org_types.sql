eba55c792de01d00bbcd3a357d3c54360f7b3dd2
abuddenberg <andrew.buddenberg@noaa.gov>
2013-07-16 14:36:36 -0400

    Added organization types patch

---
+++ db/patches/0140_org_types.sql
@@ -0,0 +1,9 @@
+insert into organization_type(identifier) values ('ngo');
+insert into organization_type(identifier) values ('federal');
+insert into organization_type(identifier) values ('municipal');
+insert into organization_type(identifier) values ('state');
+insert into organization_type(identifier) values ('research');
+insert into organization_type(identifier) values ('commercial');
+insert into organization_type(identifier) values ('private');
+insert into organization_type(identifier) values ('academic');
+
