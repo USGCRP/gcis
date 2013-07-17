e43f27bad9e30f4071d4c4acffdb67866d7d4911
Brian Duggan <bduggan@usgcrp.gov>
2013-07-17 11:43:35 -0400

    made org_type patch idempotent

--- db/patches/0140_org_types.sql
+++ db/patches/0140_org_types.sql
@@ -1,9 +1,8 @@
-insert into organization_type(identifier) values ('ngo');
-insert into organization_type(identifier) values ('federal');
-insert into organization_type(identifier) values ('municipal');
-insert into organization_type(identifier) values ('state');
-insert into organization_type(identifier) values ('research');
-insert into organization_type(identifier) values ('commercial');
-insert into organization_type(identifier) values ('private');
-insert into organization_type(identifier) values ('academic');
-
+insert into organization_type (identifier) select 'ngo'        where not exists(select 1 from organization_type where identifier='ngo');
+insert into organization_type (identifier) select 'federal'    where not exists(select 1 from organization_type where identifier='federal');
+insert into organization_type (identifier) select 'municipal'  where not exists(select 1 from organization_type where identifier='municipal');
+insert into organization_type (identifier) select 'state'      where not exists(select 1 from organization_type where identifier='state');
+insert into organization_type (identifier) select 'research'   where not exists(select 1 from organization_type where identifier='research');
+insert into organization_type (identifier) select 'commercial' where not exists(select 1 from organization_type where identifier='commercial');
+insert into organization_type (identifier) select 'private'    where not exists(select 1 from organization_type where identifier='private');
+insert into organization_type (identifier) select 'academic'   where not exists(select 1 from organization_type where identifier='academic');
