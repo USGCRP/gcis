770eaa065c68b927f46968ee80df9402fdbd52ad
Brian Duggan <bduggan@usgcrp.gov>
2013-07-26 12:35:25 -0400

    audit for new tables

---
+++ db/patches/0200_more_audit.sql
@@ -0,0 +1,9 @@
+
+select audit.audit_table('organization');
+select audit.audit_table('organization_type');
+select audit.audit_table('organization_type_map');
+select audit.audit_table('keyword');
+select audit.audit_table('finding_keyword_map');
+select audit.audit_table('image_figure_map');
+select audit.audit_table('publication_map');
+
