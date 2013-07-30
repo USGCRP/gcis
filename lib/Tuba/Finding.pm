c16e21144092f6a5484834f27aa8c6bbeb6247f7
Brian Duggan <bduggan@usgcrp.gov>
2013-07-30 10:44:05 -0400

    dropdown for chapters for findings

--- lib/Tuba/Finding.pm
+++ lib/Tuba/Finding.pm
@@ -42,5 +42,24 @@ sub show {
     );
 }
 
+sub create_form {
+    my $c = shift;
+    my @chapters = @{ Chapters->get_objects(query => [ report => $c->stash('report_identifier') ], sort_by => 'number') };
+    $c->stash(
+        controls => {
+            chapter => {
+                template => 'select',
+                params   => {
+                    values => [ '',
+                        map [ sprintf( '%s %s', ( $_->number || '' ), $_->title ), $_->identifier ], @chapters
+                    ]
+                }
+            }
+        },
+    );
+
+    $c->SUPER::create_form;
+}
+
 1;
 
