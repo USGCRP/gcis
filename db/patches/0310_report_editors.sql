7427eec2cd191f0b5589afa776c84e95b59eaf1d
Brian Duggan <bduggan@usgcrp.gov>
2013-08-27 15:48:27 -0400

    report editors

---
+++ db/patches/0310_report_editors.sql
@@ -0,0 +1,8 @@
+
+insert into _report_editor (report,username) select identifier,'bduggan@usgcrp.gov' from report where identifier like 'nca3%';
+insert into _report_editor (report,username) select identifier,'andrew.buddenberg@noaa.gov' from report where identifier like 'nca3%';
+insert into _report_editor (report,username) select identifier,'ctilmes@usgcrp.gov' from report where identifier like 'nca3%';
+insert into _report_editor (report,username) select identifier,'saulenbach@usgcrp.gov' from report where identifier like 'nca3%';
+insert into _report_editor (report,username) select identifier,'jgoldstein@usgcrp.gov' from report where identifier like 'nca3%';
+
+
