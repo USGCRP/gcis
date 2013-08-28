2886db09873344206e0ca8e6be6350d27e396fdb
Brian Duggan <bduggan@usgcrp.gov>
2013-08-28 11:01:47 -0400

    version 0.48

--- lib/Tuba.pm
+++ lib/Tuba.pm
@@ -18,7 +18,7 @@ use Date::Parse qw/str2time/;
 use Tuba::Converter;
 use Tuba::Log;
 
-our $VERSION = '0.47';
+our $VERSION = '0.48';
 our @supported_formats = qw/json ttl html nt rdfxml dot rdfjson jsontriples svg/;
 
 sub startup {
