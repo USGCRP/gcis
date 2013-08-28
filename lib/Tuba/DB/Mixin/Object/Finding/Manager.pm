2d0ebd4e377efdfa70b42af96889452fe646f744
Brian Duggan <bduggan@usgcrp.gov>
2013-08-28 10:52:20 -0400

    and findings

--- lib/Tuba/DB/Mixin/Object/Finding/Manager.pm
+++ lib/Tuba/DB/Mixin/Object/Finding/Manager.pm
@@ -10,23 +10,28 @@ sub dbgrep {
 
     my $query_string = $a{query_string} or return;
     my $limit = $a{limit} || 10;
+    my $user = $a{user};
 
-    my @found = $self->SUPER::dbgrep(%a);
+    my @query = $self->_make_query($query_string);
 
-    # Also find findings with keywords that match.
-    my @query;
-    for my $col (Tuba::DB::Object::Keyword->meta->columns) {
-        next unless $col->type =~ /char/i;
-        push @query, $col->accessor_method_name => { ilike => '%'.$query_string.'%' };
+    my @viewable;
+    my @with = ( 'report_obj' );
+    if ($user) {
+        @viewable = ( or => [ _public => 't', username => $user ] );
+        push @with, 'report_obj._report_viewer';
+    } else {
+        @viewable = ( _public => 't' );
     }
-    my $new = Tuba::DB::Object::Keyword::Manager->get_objects( query => [ or => \@query ], require_objects => [qw/finding_objs/], limit => $limit );
-    my %seen = map { $_->identifier => 1 } @found;
-    for (@$new) {
-        for ($_->finding_objs) {
-            push @found, $_ unless $seen{$_->identifier}++;
-        }
-    }
-    return @found;
+
+    my $found = $self->get_objects(
+        query => [
+             or => \@query,
+             or => \@viewable
+        ],
+        with_objects => \@with,
+        limit => $limit );
+
+    return @$found;
 }
 
 1;
