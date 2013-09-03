3fc8c7a327e129591772379bf9b1a987d602cb88
Brian Duggan <bduggan@usgcrp.gov>
2013-09-03 16:05:10 -0400

    formatting, cacade updates

---
+++ db/patches/0390_article_fk.sql
@@ -0,0 +1,7 @@
+update journal set title = 'BioScience' where title = ' BioScience';
+update journal set identifier=title where identifier='';
+
+alter table article drop constraint article_ibfk_1,
+    add constraint article_ibfk_1 foreign key (journal) 
+        references journal(identifier) on delete cascade on update cascade;
+
