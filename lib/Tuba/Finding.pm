e38b9115d48a66e7a2e5b07e5cc588d7447fce72
Brian Duggan <bduggan@usgcrp.gov>
2013-09-05 09:47:07 -0400

    fix findings

--- lib/Tuba/Finding.pm
+++ lib/Tuba/Finding.pm
@@ -12,34 +12,34 @@ sub list {
     my $c = shift;
     my $objects;
     my $meta = Finding->meta;
-    my $report = $c->stash('report_identifier');
+    my $report_identifier = $c->stash('report_identifier');
     my $all = $c->param('all');
     my @page = $all ? () : (page => $c->page);
 
-    if (my $ch = $c->stash('chapter_identifier')) {
+    if (my $chapter_identifier = $c->stash('chapter_identifier')) {
         $objects = Findings->get_objects(
-            query => [chapter => $ch, report => $report],
+            query => [chapter_identifier => $chapter_identifier, report_identifier => $report_identifier],
             with_objects => ['chapter'],
             @page,
             sort_by => "ordinal, t1.identifier");
-        $c->title("Findings in report : $report, chapter $ch");
+        $c->title("Findings in report : $report_identifier, chapter $chapter_identifier");
         $c->set_pages(
             Findings->get_objects_count(
-                query => [chapter => $ch, report => $report],
+                query => [chapter_identifier => $chapter_identifier, report_identifier => $report_identifier],
                 with_objects => ['chapter'],
             )
         ) unless $all;
     } else {
         $objects = Findings->get_objects(
-            query => [ report => $report ],
+            query => [ report_identifier => $report_identifier ],
             with_objects => ['chapter'],
             sort_by => "ordinal, t1.identifier",
             @page,
         );
         $c->set_pages(
-            Findings->get_objects_count( query => [ report => $report ] )
+            Findings->get_objects_count( query => [ report_identifier => $report_identifier ] )
         ) unless $all;
-        $c->title("Findings in report $report");
+        $c->title("Findings in report $report_identifier");
     }
 
     $c->stash(objects => $objects);
@@ -50,8 +50,8 @@ sub show {
     my $c = shift;
     my $meta = Finding->meta;
     my $identifier = $c->stash('finding_identifier');
-    my $report = $c->stash('report_identifier');
-    my $object = Finding->new( identifier => $identifier, report => $report )->load( speculative => 1)
+    my $report_identifier = $c->stash('report_identifier');
+    my $object = Finding->new( identifier => $identifier, report_identifier => $report_identifier )->load( speculative => 1)
       or return $c->render_not_found;
     $c->stash(object => $object);
     $c->respond_to(
