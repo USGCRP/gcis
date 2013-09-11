1e2c619e1d44cedbfb6ad51e441355d5fc709365
Brian Duggan <bduggan@usgcrp.gov>
2013-09-11 14:34:59 -0400

    dbdist, docs

--- db/dist/base.sql
+++ db/dist/base.sql
@@ -27,7 +27,7 @@ CREATE FUNCTION update_publication() RETURNS trigger
     AS $$
 BEGIN
     update publication set fk = slice(hstore(NEW.*),akeys(fk))
-         where publication_type = TG_TABLE_NAME::text and
+         where publication_type_identifier = TG_TABLE_NAME::text and
             fk = slice(hstore(OLD.*),akeys(fk));
      RETURN NEW;
 END; $$;
