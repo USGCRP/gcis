bd6ffb1f603ce02ec691c1d245c0c665c34da7e6
Brian Duggan <bduggan@usgcrp.gov>
2013-10-16 12:26:50 -0600

    more updates

--- eg/report_thumbnails.pl
+++ eg/report_thumbnails.pl
@@ -26,30 +26,51 @@ if ($identifier) {
     $all = $res->json;
 }
 
+sub done {
+    my $identifier = shift;
+    my $tx = $ua->get("$base/report/$identifier.json");
+    my $res = $tx->success or do { warn $tx->error; return; };
+    return 1 if $res->json->{files} && @{ $res->json->{files} };
+    return 0;
+}
+
 my $dir;
 my $errs;
 my $ok;
 my $out;
+my $done;
 for (@$all) {
     $ok = 0;
+    $done = 0;
     $dir = File::Temp->newdir;
     chdir $dir;
     my $identifier = $_->{identifier};
+    say "# $identifier :";
+    do { $done=1; next; } if done($identifier);
     my $url = $_->{url} or next;
     $errs = File::Temp->new;
-    say "Doing $identifier ($url)";
-    $out = `curl -q '$url' > ./out.pdf 2>$errs`;
+    $out = `curl -m300 -q '$url' > ./out.pdf 2>$errs`;
     -e './out.pdf' or next;
-    $out = `gm convert -resize 600x400 out.pdf[0] out.jpg 2>$errs`;
-    -e './out.jpg' or next;
-    $out = `curl -T ./out.jpg -u $creds $base/report/files/$identifier 2> $errs`;
+    chomp(my $type = `file ./out.pdf`);
+    unless ($type =~ /PDF/) {
+        $out = "File is : $type";
+        undef $errs;
+        next;
+    }
+    $out = `gm convert -resize 600x400 out.pdf[0] cover.png 2>$errs`;
+    -e './cover.png' or next;
+    $out = `curl -T ./cover.png -u $creds $base/report/files/$identifier/ 2> $errs`;
     $ok = 1 unless $?;
 } continue {
     chdir '/tmp';
-    if (!$ok) {
-        say "\n\n$out\nerrors : ", file("$errs")->slurp;
-        say "fail";
+    if ($done) {
+        say "# skipped";
+    } elsif (!$ok) {
+        say "# $out";
+        say "# errors : ", file("$errs")->slurp if $errs && -e $errs;
+        say "not ok";
     }
+    say "ok";
     undef $dir;
     undef $errs;
 }
