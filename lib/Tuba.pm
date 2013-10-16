33d08c2aedfc81f774a8b96cf4c193272282cc9e
Brian Duggan <bduggan@usgcrp.gov>
2013-10-15 21:49:29 -0400

    version 0.53

--- lib/Tuba.pm
+++ lib/Tuba.pm
@@ -19,7 +19,7 @@ use Tuba::Converter;
 use Tuba::Log;
 use Data::UUID::LibUUID;
 
-our $VERSION = '0.52';
+our $VERSION = '0.53';
 our @supported_formats = qw/json ttl html nt rdfxml dot rdfjson jsontriples svg/;
 
 sub startup {
