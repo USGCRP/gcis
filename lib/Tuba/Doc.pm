cc396f72155b0572b3d060aec09e231977081d71
Brian Duggan <bduggan@usgcrp.gov>
2013-10-21 16:48:02 -0400

    examples

--- lib/Tuba/Doc.pm
+++ lib/Tuba/Doc.pm
@@ -11,6 +11,7 @@ use Mojo::Base qw/Tuba::Controller/;
 sub examples {
     my $c = shift;
     my $sparql_url = Mojo::URL->new(q[http://data.gcis-dev-front.joss.ucar.edu/sparql]);
+    my $base = "http://data.gcis-dev-front.joss.ucar.edu";
 
     my $sparql = [
         { desc => "Get a list of figures in the GCIS triplestore.",
@@ -25,7 +26,16 @@ limit 10",
 where { ?s a gcis:Finding }
 limit 10",
         },
-
+        {
+          desc => "Find all publications that informed figure 2.26 : global-slr",
+code =>
+"select ?y FROM <http://data.globalchange.gov>
+where {
+ <$base/report/nca3draft/chapter/our-changing-climate/figure/global-slr> gcis:hasImage ?img .
+ ?img prov:wasInformedBy ?y
+}
+limit 10"
+}
     ];
     for (@$sparql) {
         my $url = $sparql_url->clone;
