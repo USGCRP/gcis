faa0306a3959a538899cc9aa04c31f840c842d12
Brian Duggan <bduggan@usgcrp.gov>
2014-02-07 08:17:58 -0500

    doi cleanup

---
+++ db/patches/0790_doi_check.sql
@@ -0,0 +1,8 @@
+delete from article where doi !~ '^10.[[:print:]]+/[[:print:]]+$';
+
+alter table article add constraint article_doi_check check (doi ~ '^10.[[:print:]]+/[[:print:]]+$');
+
+update article set identifier=doi where not (identifier = doi or identifier similar to '[a-z0-9_-]+');
+
+alter table article add constraint article_identifier_check check (identifier = doi or identifier similar to '[a-z0-9_-]+');
+
