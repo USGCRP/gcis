bef56489d6f5b70e03f67f305d3c846575d4a4cc
Brian Duggan <bduggan@usgcrp.gov>
2014-02-07 08:28:26 -0500

    better still

--- db/patches/0790_doi_check.sql
+++ db/patches/0790_doi_check.sql
@@ -1,8 +1,10 @@
 delete from article where doi !~ '^10.[[:print:]]+/[[:print:]]+$';
 
+delete from article where not (identifier similar to '[a-z0-9_-]+' or identifier ~ '^10.[[:print:]]+/[[:print:]]+$');
+
 alter table article add constraint article_doi_check check (doi ~ '^10.[[:print:]]+/[[:print:]]+$');
 
 update article set identifier=doi where not (identifier = doi or identifier similar to '[a-z0-9_-]+');
 
-alter table article add constraint article_identifier_check check (identifier similar to '[a-z0-9_-]+' or identifier ~ '10.[[:print:]]+/[[:print:]]+');
+alter table article add constraint article_identifier_check check (identifier similar to '[a-z0-9_-]+' or identifier ~ '^10.[[:print:]]+/[[:print:]]+$');
 
