d5e9f04bc2025d82cdfa372790918f8441634c88
Brian Duggan <bduggan@usgcrp.gov>
2014-03-17 14:11:36 -0400

    constraints for ordinals

--- db/patches/0960_figure_ordinal.sql
+++ db/patches/0960_figure_ordinal.sql
@@ -1,2 +1,4 @@
 alter table figure add unique (report_identifier, chapter_identifier, ordinal);
+alter table finding add unique (report_identifier, chapter_identifier, ordinal);
+alter table "table" add unique (report_identifier, chapter_identifier, ordinal);
 
