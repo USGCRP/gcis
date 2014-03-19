9ccf102e1cdd6f32d19330a92b7ca32847e3ff40
Brian Duggan <bduggan@usgcrp.gov>
2014-03-19 17:03:42 -0400

    handle undefs in human_duration

--- lib/Tuba/Plugin/TubaHelpers.pm
+++ lib/Tuba/Plugin/TubaHelpers.pm
@@ -325,6 +325,7 @@ sub register {
     $app->helper(human_duration => sub {
             my $c = shift;
             my $val = shift;
+            return "" unless defined($val) && length($val);
             return DateTime::Format::Human::Duration->new()->format_duration($val);
         });
     $app->helper(default_html_relationships => sub {
