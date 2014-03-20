7f48a138484df35db9948c0c7979dfa402d3e8bc
Brian Duggan <bduggan@usgcrp.gov>
2014-03-20 18:18:05 -0400

    preserve methodologies

--- db/patches/0950_publication_cleanup.sql
+++ db/patches/0950_publication_cleanup.sql
@@ -1,8 +1,10 @@
+
 delete from report where identifier in (
     select g.identifier from report g
     inner join publication p on p.publication_type_identifier='report' and p.fk->'identifier' = g.identifier
     left join reference r on r.child_publication_id = p.id
-    where r.identifier is null
+    left join methodology m on m.publication_id = p.id
+    where r.identifier is null and m.publication_id is null
     and g.identifier != 'nca3'
 );
 
@@ -10,21 +12,24 @@ delete from webpage where identifier in (
     select g.identifier from webpage g
     inner join publication p on p.publication_type_identifier='webpage' and p.fk->'identifier' = g.identifier
     left join reference r on r.child_publication_id = p.id
-    where r.identifier is null
+    left join methodology m on m.publication_id = p.id
+    where r.identifier is null and m.publication_id is null
 );
 
 delete from book where identifier in (
     select g.identifier from book g
     inner join publication p on p.publication_type_identifier='book' and p.fk->'identifier' = g.identifier
     left join reference r on r.child_publication_id = p.id
-    where r.identifier is null
+    left join methodology m on m.publication_id = p.id
+    where r.identifier is null and m.publication_id is null
 );
 
 delete from article where identifier in (
     select g.identifier from article g
     inner join publication p on p.publication_type_identifier='article' and p.fk->'identifier' = g.identifier
     left join reference r on r.child_publication_id = p.id
-    where r.identifier is null
+    left join methodology m on m.publication_id = p.id
+    where r.identifier is null and m.publication_id is null
 );
 
 delete from image where identifier in (
