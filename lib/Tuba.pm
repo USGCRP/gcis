608ebbeebc1df18b8553075a117ff603382167a2
Brian Duggan <bduggan@usgcrp.gov>
2014-03-20 18:08:29 -0400

    version 0.67

--- lib/Tuba.pm
+++ lib/Tuba.pm
@@ -51,7 +51,7 @@ use Tuba::Log;
 use Tuba::Util qw/set_config/;
 use Data::UUID::LibUUID;
 
-our $VERSION = '0.66';
+our $VERSION = '0.67';
 our @supported_formats = qw/json yaml ttl html nt rdfxml dot rdfjson jsontriples svg txt/;
 
 sub startup {
