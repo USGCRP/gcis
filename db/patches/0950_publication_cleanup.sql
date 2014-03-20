19441482c4e1796eba7722b2ab8e55108157e079
Brian Duggan <bduggan@usgcrp.gov>
2014-03-20 10:14:36 -0400

    publication cleanup

---
+++ db/patches/0950_publication_cleanup.sql
@@ -0,0 +1,36 @@
+delete from report where identifier in (
+    select g.identifier from report g
+    inner join publication p on p.publication_type_identifier='report' and p.fk->'identifier' = g.identifier
+    left join reference r on r.child_publication_id = p.id
+    where r.identifier is null
+    and g.identifier != 'nca3'
+);
+
+delete from webpage where identifier in (
+    select g.identifier from webpage g
+    inner join publication p on p.publication_type_identifier='webpage' and p.fk->'identifier' = g.identifier
+    left join reference r on r.child_publication_id = p.id
+    where r.identifier is null
+);
+
+delete from book where identifier in (
+    select g.identifier from book g
+    inner join publication p on p.publication_type_identifier='book' and p.fk->'identifier' = g.identifier
+    left join reference r on r.child_publication_id = p.id
+    where r.identifier is null
+);
+
+delete from article where identifier in (
+    select g.identifier from article g
+    inner join publication p on p.publication_type_identifier='article' and p.fk->'identifier' = g.identifier
+    left join reference r on r.child_publication_id = p.id
+    where r.identifier is null
+);
+
+delete from image where identifier in (
+    select identifier from image i
+    left join image_figure_map f
+    on f.image_identifier = i.identifier
+    where f.image_identifier is null
+);
+
