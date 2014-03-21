94d1ff4df8b631c2936a2d9abb6ee6f9d95af9f3
Brian Duggan <bduggan@usgcrp.gov>
2014-03-21 12:50:56 -0400

    activity audits

--- db/patches/0970_activity.sql
+++ db/patches/0970_activity.sql
@@ -4,3 +4,7 @@ update activity set identifier = lower(translate(identifier,' ','-'))
 alter table activity add constraint ck_activity_identifier
     check (identifier similar to '[a-z0-9_-]+');
 
+select audit.audit_table('activity');
+
+select audit.audit_table('methodology');
+
