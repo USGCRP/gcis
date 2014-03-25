15d62fb108f4d442cef50a4c37f6a64208a3597d
Brian Duggan <bduggan@usgcrp.gov>
2014-03-25 11:41:05 -0400

    report years

---
+++ db/patches/0980_report_fields.sql
@@ -0,0 +1,9 @@
+alter table report add column publication_year integer;
+alter table report add constraint ck_report_pubyear check (publication_year > 0 and publication_year < 9999);
+
+update report g
+    set publication_year = (r.attrs->'Year')::integer
+from
+    publication p inner join reference r on r.child_publication_id = p.id
+    where p.publication_type_identifier='report' and p.fk->'identifier' = g.identifier
+
