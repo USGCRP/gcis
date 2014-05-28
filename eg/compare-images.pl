a029ec403d674ac06b711f22f0ebee5625ec5324
rewolfe <rewolfe@usgcrp.gov>
2014-05-28 11:39:47 -0400

    Update compare-images.pl
    fixed bug line 11

--- eg/compare-images.pl
+++ eg/compare-images.pl
@@ -6,16 +6,52 @@ use strict;
 
 $| = 1;
 
-my $ua = Mojo::UserAgent->new;
+my $line = <STDIN>;
+$line =~ s/\r//;
+chomp($line);
+my $report = $line;
+print "report $report\n";
 
-my $report = "nca3"
+my @ids = ();
+my @titles = ();
 
-my $images = $ua->get('http://data.globalchange.gov/report/$report/image.json')->res->json;
-
-for my $image (@$images) {
-    my $id = $image->{identifier};
-    my $title = $image->{title};
-    
-    print "image $id  title $title\n";
-    sleep 0.01;
+foreach $line (<STDIN>) {
+    $line =~ s/\r//;
+    chomp($line);
+    # print "line $line\n";
+    my ($title, $id) = split(/\t/, $line);
+    # print "id $id  title $title\n";
+    push @ids, $id;
+    push @titles, $title;
 }
+print "checking $#ids figures\n";
+
+my %index_id = map {$ids[$_] => $_} 0..$#ids;
+# print Dumper %index_id;
+
+my $base = "http://data-stage.globalchange.gov";
+my $param = "?all=1";
+my $url = "$base/report/$report/figure.json$param";
+# print "url $url\n";
+
+my $ua = Mojo::UserAgent->new;
+my $figures = $ua->get($url)->res->json;
+# print Dumper $figures;
+
+for my $figure (@$figures) {
+    my $id = $figure->{identifier};
+    my $title = $figure->{title};
+    
+    my $index = $index_id{$id};
+    # print "id $id  index $index\n";
+    if(!$index) {
+        print "  id $id not found\n";
+        next;
+    }
+    if($titles[$index] != $title) {
+        print "  titles do not match for id $id\n";
+        print "    1: $titles[$index]\n";
+        print "    2: $title\n";
+    }
+}
+print "done\n";
