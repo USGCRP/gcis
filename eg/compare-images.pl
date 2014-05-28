818e62a04def54e73bf39a272c4ba5511703fa1d
rewolfe <rewolfe@usgcrp.gov>
2014-05-28 10:44:44 -0400

    Update compare-images.pl
    first try

--- eg/compare-images.pl
+++ eg/compare-images.pl
@@ -8,15 +8,14 @@ $| = 1;
 
 my $ua = Mojo::UserAgent->new;
 
-my $articles = $ua->get('http://data.globalchange.gov/article.json')->res->json;
+my $report = "nca3"
 
-for my $article (@$articles) {
-    my $url = $article->{url};
-    my $doi = $article->{doi};
-    next unless $doi && $url;
-    next if $url =~ /dx.doi.org/;
-    my $doi_redirect = $ua->get("http://dx.doi.org/$doi")->res->headers->location;
-    next if $url eq $doi_redirect;
-    print "Different urls for $doi : $url $doi_redirect\n";
-    sleep 0.2;
+my $images = $ua->get('http://data.globalchange.gov/report/nca3/image.json')->res->json;
+
+for my $image (@$images) {
+    my $id = $image->{identifier};
+    my $title = $image->{title};
+    
+    print "image $id  title $title\n";
+    sleep 0.01;
 }
