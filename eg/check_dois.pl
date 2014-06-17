8ddb6867b0cc27a2ea1677007100268142c0e4d6
rewolfe <rewolfe@usgcrp.gov>
2014-06-17 16:04:40 -0400

    Create check_dois.pl
    Checking DOIs (just a start)

---
+++ eg/check_dois.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+
+
+use Mojo::UserAgent;
+use Data::Dumper;
+use strict;
+
+
+$| = 1;
+
+
+my $ua = Mojo::UserAgent->new;
+
+
+my $articles = $ua->get('http://data.globalchange.gov/article.json')->res->json;
+
+my $i = 0;
+for my $article (@$articles) {
+    my $url = $article->{url};
+    my $doi = $article->{doi};
+    next unless $doi && $url;
+    next if $url =~ /dx.doi.org/;
+    my $doi_redirect = $ua->get("http://dx.doi.org/$doi")->res->headers->location;
+    next if $url eq $doi_redirect;
+    print "Different urls for $doi : $url $doi_redirect\n";
+
+    last if $i > 10;
+    $i++;
+}
