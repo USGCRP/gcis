8e7dbb45d4487b15ae018e128f298f48d6df51dc
Justin Goldstein <jgoldstein@usgcrp.gov>
2014-09-12 08:51:06 -0600

    removed excess whitespace in various lines on TubaHelper.pm

--- lib/Tuba/Plugin/TubaHelpers.pm
+++ lib/Tuba/Plugin/TubaHelpers.pm
@@ -504,7 +504,7 @@ sub register {
           bibo     => 'http://purl.org/ontology/bibo/',
           dbpedia  => 'http://dbpedia.org/resource/',
           dbpprop => 'http://dbpedia.org/property/',
-          dbpedia_owl => 'http://dbpedia.org/ontology/', 
+          dbpedia_owl => 'http://dbpedia.org/ontology/',
           dc       => 'http://purl.org/dc/elements/1.1/',
           dcterms  => 'http://purl.org/dc/terms/',
           dcmitype => 'http://purl.org/dc/dcmitype/',
@@ -531,7 +531,7 @@ sub register {
           fabio  => 'http://purl.org/spar/fabio/',
           schema => 'http://schema.org/',
           skos   => 'http://www.w3.org/2004/02/skos/core#',
-          place => 'http://purl.org/ontology/places#',   
+          place => 'http://purl.org/ontology/places#',
      )
     });
 }
