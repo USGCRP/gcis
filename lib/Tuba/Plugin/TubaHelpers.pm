bb21a3145e4f8bbca09f085b6fbbacf22abee7aa
Justin Goldstein <jgoldstein@usgcrp.gov>
2014-10-29 08:04:39 -0600

    reverted TubaHelpers.pm

--- lib/Tuba/Plugin/TubaHelpers.pm
+++ lib/Tuba/Plugin/TubaHelpers.pm
@@ -495,7 +495,6 @@ sub register {
                            (?:[a-zA-Z]+) # optional type
                         )?
                         ;$                    # end of line
-                        <<>>                 # URIs
                       ]x;
         });
     $app->helper(external_link => sub {
