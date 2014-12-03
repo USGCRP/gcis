da0518203dc647581897f2df0be59591f879af2b
Brian Duggan <bduggan@usgcrp.gov>
2014-12-03 13:01:34 -0500

    added comment back

--- db/patches/2030_model_runs.sql
+++ db/patches/2030_model_runs.sql
@@ -3,3 +3,4 @@ alter table model_run add column time_resolution interval;
 alter table model_run drop constraint model_run_model_identifier_scenario_identifier_spatial_reso_key;
 alter table model_run add constraint model_run_unique unique (model_identifier, scenario_identifier, range_start, range_end, sequence, time_resolution);
 
+comment on column "model_run".time_resolution is 'The temporal resolution of this run.';
