8fe936582500aa9f2df3653aa56a99c08cbb4a1b
Brian Duggan <bduggan@usgcrp.gov>
2015-03-05 11:31:40 -0500

    turtle syntax check without rapper

--- t/tlib.pm
+++ t/tlib.pm
@@ -1,35 +1,28 @@
 no warnings 'redefine';
+use RDF::Trine::Parser;
 
-sub Mojo::UserAgent::Transactor::redirect {
-  my ($self, $old) = @_;
+sub d($) {
+    Test::More::diag(shift);
+}
 
-  # Commonly used codes
-  my $res = $old->res;
-  my $code = $res->code // '';
-  return undef unless grep { $_ eq $code } 301, 302, 303, 307, 308;
+sub import {
+    no strict 'refs';
+    *{[caller]->[0].'::turtle_ok'} = \&turtle_ok;
+}
 
-  # Fix broken location without authority and/or scheme
-  return unless my $location = $res->headers->location;
-  $location = Mojo::URL->new($location);
-  $location = $location->base($old->req->url)->to_abs unless $location->is_abs;
+sub turtle_ok {
+    my $ttl = shift;
+    my $parser = RDF::Trine::Parser->new('turtle');
+    eval {
+        $parser->parse("http://test.data.globalchange.gov", $ttl, sub { 1; } );
+    };
 
-  # Clone request if necessary
-  my $new    = Mojo::Transaction::HTTP->new;
-  my $req    = $old->req;
-  my $method = uc $req->method;
-  if ($code eq 301 || $code eq 307 || $code eq 308) {
-    return undef unless my $req = $req->clone;
-    $new->req($req);
-    $method = 'GET' if $code eq 303 and $method ne 'HEAD';
-    $req->headers->remove('Host')->remove('Cookie')->remove('Referer');
-  } elsif ($method ne 'HEAD') { $method = 'GET' }
-
-  if (defined(my $accept = $req->headers->accept)) {
-     $new->req->headers->accept($accept);
-  }
-
-  $new->req->method($method)->url($location);
-  return $new->previous($old);
+    if ($@) {
+        d "Error parsing turtle :\n$@";
+        d "Turtle : ";
+        d $ttl;
+    }
+    Test::More::ok(!$@, "Parsed turtle");
 }
 
 1;
