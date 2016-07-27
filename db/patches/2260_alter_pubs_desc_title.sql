1a75d9e19d8a15c51e1a7abb9e2dccc63129e5fa
Kathryn Tipton <ktipton@usgcrp.gov>
2016-07-27 15:53:02 -0400

    gcis#369 create column description for 'article' and 'webpage'

---
+++ db/patches/2260_alter_pubs_desc_title.sql
@@ -0,0 +1,8 @@
+/* Extending some publication types to have a description & title column. See github gcis#369 */
+
+alter table article add column description character varying;
+alter table webpage add column description character varying;
+
+
+comment on column "article".description is 'The abstract of the article, or a similar brief description.';
+comment on column "webpage".description is 'A brief description of the web page.';
