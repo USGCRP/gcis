291016f365a36100d2a6f815e6b08587a70c5cf0
Kathryn Tipton <ktipton@usgcrp.gov>
2016-07-29 09:28:23 -0400

    gcis#369 create column description for 'chapter'

--- db/patches/2260_alter_pubs_desc_title.sql
+++ db/patches/2260_alter_pubs_desc_title.sql
@@ -2,7 +2,9 @@
 
 alter table article add column description character varying;
 alter table webpage add column description character varying;
+alter table chapter add column description character varying;
 
 
 comment on column "article".description is 'The abstract of the article, or a similar brief description.';
 comment on column "webpage".description is 'A brief description of the web page.';
+comment on column "chapter".description is 'A brief description of the chapter.';
