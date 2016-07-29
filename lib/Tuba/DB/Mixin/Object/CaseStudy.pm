a99d04cb840871f0a85515e9f2d80f0e67c1379d
Randall Sindlinger <rsindlin@gmail.com>
2016-07-29 12:34:20 +0000

    CaseStudy and Toolkit JSON keys match Webpage
    The original idea was that Toolkits and Case Studies would be a
    type of webpage, so forcing the keys to match those displayed for a
    webpage.

--- lib/Tuba/DB/Mixin/Object/CaseStudy.pm
+++ lib/Tuba/DB/Mixin/Object/CaseStudy.pm
@@ -6,4 +6,25 @@ sub stringify {
     return $self->description;
 }
 
+sub as_tree {
+    my $self = shift;
+    my $id = $self->gcid;
+    my $id =~ s!.*//!!;
+    my $default_tree = $self->SUPER::as_tree;
+    my %tree = (
+             %$default_tree,
+             url => $self->gcid,
+             identifier => $id,
+             title => $self->stringify,
+             access_date => $self->timestamp,
+             uri => '/case_study/',
+                );
+    my @keys_not_in_webpage = qw/relationship_identifier lexicon_identifier term context_identifier term_identifier
+                                 timestamp gcid/ ;
+    for my $key_not_in_webpage (@keys_not_in_webpage) {
+        delete $tree{$key_not_in_webpage};
+    }
+    return \%tree;
+}
+
 1;
--- lib/Tuba/DB/Mixin/Object/Toolkit.pm
+++ lib/Tuba/DB/Mixin/Object/Toolkit.pm
@@ -1,9 +1,32 @@
 package Tuba::DB::Object::Toolkit;
 use strict;
+use Tuba::Log;
 
 sub stringify {
     my $self = shift;
     return $self->description;
+}
+
+sub as_tree {
+    my $self = shift;
+    my $id = $self->gcid;
+    my $id =~ s!.*//!!;
+    my $default_tree = $self->SUPER::as_tree;
+    my %tree = (
+             %$default_tree,
+             url => $self->gcid,
+             identifier => $id,
+             title => $self->stringify,
+             access_date => $self->timestamp,
+             uri => '/toolkit/',
+                );
+    my @keys_not_in_webpage = qw/relationship_identifier lexicon_identifier term context_identifier term_identifier
+                                 timestamp gcid/ ;
+    for my $key_not_in_webpage (@keys_not_in_webpage) {
+        delete $tree{$key_not_in_webpage};
+    }
+    return \%tree;
+
 }
 
 1;
