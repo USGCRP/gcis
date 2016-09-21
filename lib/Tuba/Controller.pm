USGCRP/gcis

commit 19ea2fd1d0678e8553c2154ce6e7a84d4e07a86a

Add parents array as alternative to cited_by array
Reports use parents array with cito:isCitedBy, but Articles (and other
resources using the generic Controller::make_tree_for_show) provide
a cited_by array.

lib/Tuba/Controller.pm
@@ -220,8 +220,12 @@ sub make_tree_for_show {
                     publication_type => $_->{publication_type_identifier},
                 }, @pubs
             ];
+            my @parents;
+            push @parents, $pub->get_parents_with_references(uniq => 1, for_export => 1, c => $c);
+            $ret->{parents} = \@parents;
         } else {
             $ret->{cited_by} = [];
+            $ret->{parents} = [];
         }
     }
     $ret;

commit 5c011f26c5a139fbafbbebb282b5ac794029be44

Don't overwrite JSON parents if already populated
Test 007_prov.t was failing with an empty parents array.  Bug
introduced in commit 19ea2fd1d0678e8553c2154ce6e7a84d4e07a86a.

lib/Tuba/Controller.pm
@@ -221,12 +221,14 @@ sub make_tree_for_show {
                     publication_type => $_->{publication_type_identifier},
                 }, @pubs
             ];
-            my @parents;
+            #Parents array added as alternative to cited_by array (because Reports already use
+            #a parents array with cito:isCitedBy)
+            my @parents = @{$ret->{parents}};
             push @parents, $pub->get_parents_with_references(uniq => 1, for_export => 1, c => $c);
             $ret->{parents} = \@parents;
         } else {
             $ret->{cited_by} = [];
-            $ret->{parents} = [];
+            $ret->{parents} //= [];
         }
     }
     $ret;