c5ae47fb2c19c960eb10496324c6ac58884ea497
Randall Sindlinger <rsindlin@gmail.com>
2016-09-23 18:58:20 +0000

    Make ::Object::Term::uri like Tuba::DB::Object
    The uri method in Tuba::DB::Object::Term is overridden really only
    to use a unique key instead of the primary key of the table for the
    uri.  The code was originally a direct copy-and-paste, to prototype
    the feasibility of passing a unique-key parameter, which would
    eliminate the need for overriding the method.  Due to changes in
    url_for behavior as of Mojolicious 7.x, additional debugging is
    being added here, as was done in the superclass method.

--- lib/Tuba/DB/Mixin/Object/Term.pm
+++ lib/Tuba/DB/Mixin/Object/Term.pm
@@ -1,6 +1,7 @@
 package Tuba::DB::Object::Term;
 use strict;
 use Tuba::Log;
+use Data::Dumper;
 
 sub stringify {
     my $c = shift;
@@ -14,7 +15,7 @@ sub uri {
     my $opts = shift || {};
     my $route_name = $opts->{tab} || 'show';
     $route_name .= '_'.$s->meta->table;
-#    logger->debug("sub uri using route_name=$route_name");
+    logger->debug("--- In sub 'Tuba::DB::Object::Term::uri for route_name=$route_name---");
     return $c->url_for($route_name) unless ref $s;
 #    logger->debug("still in sub uri");
     my $table = $s->meta->table;
@@ -33,7 +34,14 @@ sub uri {
         #also add a param with the unmodified column name, specifically needed for 'term'
         $url_params{$column_name} = $pk{$column_name} unless exists $url_params{$column_name};
     }
-    return $c->url_for( $route_name, \%url_params );
+    my $url_for = $c->url_for($route_name, \%url_params );
+    if ($url_for =~ /show/) {
+        logger->warn ("Strange URI created: $url_for\n".
+                      "Route name is '$route_name'\n" .
+                      "url_params passed to url_for were " . Dumper \%url_params
+                     );
+    }
+    return $url_for;
 }
 
 
