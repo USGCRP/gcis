ace77181c1e90062721aca441b7ed85e1a0e3692
Kathryn Tipton <ktipton@usgcrp.gov>
2017-06-08 08:29:52 -0400

    Generalized Person merge to the Contributor object
    Also makes the Person merge handle conflicting PublicationContributorMaps properly
    in case they do not merge cleanly. This is done via implementing merge_into on the
    PublicationContributorMap object.
    
    Bumps #531 & #491

--- lib/Tuba/DB/Mixin/Object/Contributor.pm
+++ lib/Tuba/DB/Mixin/Object/Contributor.pm
@@ -1,5 +1,6 @@
 package Tuba::DB::Object::Contributor;
 use strict;
+use Tuba::Log;
 
 sub stringify {
     my $c = shift;
@@ -67,4 +68,64 @@ sub as_tree {
     return \%h;
 }
 
+sub merge_into {
+    my $s = shift;
+    my %args = @_;
+    my $new = $args{new};
+    my $merge_on = $args{merge_on} // 0;
+    my $audit_user = $args{audit_user};
+    my $audit_note = $args{audit_note};
+
+    # Construct our target Contributor object
+    my $target;
+    if ($merge_on eq 'person' ) {
+        $target = Tuba::DB::Object::Contributor->new(
+            role_type_identifier    => $contributor->role_type_identifier,
+            organization_identifier => $contributor->organization_identifier,
+            person_id               => $new->id,
+        );
+    } elsif ($merge_on eq 'organization' ) {
+        $target = Tuba::DB::Object::Contributor->new(
+            role_type_identifier    => $contributor->role_type_identifier,
+            organization_identifier => $new->id,
+            person_id               => $contributor->person_id,
+        );
+    } else {
+        die "Contributor must be merged based on Person or Organization";
+    }
+
+    # Discover if this target Contributor already exists
+    if ($target->load(speculative => 1)) {
+        # Case 1:
+        # Already a contributor record for this role + person + org on the target?
+        # Assign all publication maps for '$s' to '$target' and remove '$s'
+        # before:
+        #     person 1 -- contributor 1 -- map entry -- publication 1
+        #     person 2 -- contributor 2 -- map entry -- publication 2
+        # after:
+        #     person 1 --\                    /--- map entry -- publication 1  
+        #                 --- contributor 3 --
+        #     person 2 --/                    \--- map entry -- publication 2
+        my $pc_maps = Tuba::DB::Object::PublicationContributorMap::Manager->get_objects(
+            query => [ contributor_id => $s->id ]);
+        for my $pub_contr_map (@$pc_maps) {
+            $pub_contr_map->merge_into( { new => $target->id } );
+        }
+        $s->delete(audit_user => $audit_user, audit_note => $audit_note) or die $s->error;
+    } else {
+        # Case 2:
+        # Easier, just use existing contributor record but our person id.
+        # before:
+        #      person 1 -- contributor 1 --- map entries -- publications
+        # after:
+        #      person 2 -- contributor 1 --- map entries -- publications
+        if ( $merge_on eq 'person' ) {
+            $s->person_id($new->id);
+        } elsif ( $merge_on eq 'organization' ) {
+            $s->organization_id($new->id);
+        }
+        $s->save(audit_user => $audit_user, audit_note => $audit_note) or die $s->error;
+    }
+    return 1;
+}
 1;
