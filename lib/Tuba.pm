c12dafcd9bfb39ebbba0ee0b12af122f45c5dd1e
GCIS Ops <gcisops@usgcrp.gov>
2018-03-08 11:19:40 -0700

    version 1.52.2

--- lib/Tuba.pm
+++ lib/Tuba.pm
@@ -72,7 +72,7 @@ use Path::Class qw/file/;
 use Data::Rmap qw/rmap_all/;
 use strict;
 
-our $VERSION = '1.51.2';
+our $VERSION = '1.52.2';
 our @supported_formats = qw/json yaml ttl html nt rdfxml dot rdfjson jsontriples svg txt thtml csv/;
 
 sub startup {
