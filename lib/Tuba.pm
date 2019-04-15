7228e6f83de6f8867596267b3015ac9a607a6085
Kathryn Tipton <ktipton@usgcrp.gov>
2019-04-15 10:39:40 -0400

    version 1.57.1

--- lib/Tuba.pm
+++ lib/Tuba.pm
@@ -72,7 +72,7 @@ use Path::Class qw/file/;
 use Data::Rmap qw/rmap_all/;
 use strict;
 
-our $VERSION = '1.57.0';
+our $VERSION = '1.57.1';
 our @supported_formats = qw/json yaml ttl html nt rdfxml dot rdfjson jsontriples svg txt thtml csv/;
 
 sub startup {
